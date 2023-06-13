85046aab201b03735a9403655cbd7a6f6c44254a
yuri.freire <yuri_4017@hotmail.com>
2023-06-13 20:16:55 -0300

    Criando domínio do sistema

---
+++ src/Domain/Doctors/Doctor.cs
@@ -0,0 +1,11 @@
+﻿namespace Medicar.Domain.Doctors;
+
+public class Doctor
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string CRM { get; set; }
+    public string Email { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+}
---
+++ src/Domain/Doctors/Schedule.cs
@@ -0,0 +1,10 @@
+﻿namespace Medicar.Domain.Doctors;
+
+public class Schedule
+{
+    public Doctor Doctor { get; set; }
+    public DateOnly AppointmentDate { get; set; }
+    public List<TimeSpan> AppointmentTimes { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+}
