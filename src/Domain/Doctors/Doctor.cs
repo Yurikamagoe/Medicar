ee19cc7cba72c1d37f0a2b33e9d8f8b58b00520e
yuri.freire <yuri_4017@hotmail.com>
2023-06-16 17:21:22 -0300

    Adicionando contratos de notificação de médico

--- src/Domain/Doctors/Doctor.cs
+++ src/Domain/Doctors/Doctor.cs
@@ -1,8 +1,20 @@
-﻿namespace Medicar.Domain.Doctors;
+﻿using Flunt.Validations;
 
-public class Doctor :Entity
+namespace Medicar.Domain.Doctors;
+
+public class Doctor : Entity
 {
     public string Name { get; set; }
     public string CRM { get; set; }
     public string Email { get; set; }
+
+    public Doctor(string name, string crm, string email)
+    {
+        var contract = new Contract<Doctor>()
+            .IsNotNull(name, "Name", "Nome do médico é obrigatório.")
+            .IsNotNull(crm, "CRM", "CRM do médico é obrigatório.")
+            .IsNotNull(email, "Email", "Email do médico é obrigatório.")
+            .IsEmail(email, "Email do médico inválido.");
+        AddNotifications(contract);
+    }
 }
--- src/Endpoints/Doctors/DoctorPost.cs
+++ src/Endpoints/Doctors/DoctorPost.cs
@@ -7,7 +7,7 @@ namespace Medicar.Endpoints.Doctors;
 public class DoctorPost
 {
     public static string Template => "/doctors";
-    public static string[] Methods => new string[] {HttpMethod.Post.ToString()};
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
 
     public static IResult Action(DoctorRequest doctorRequest, ApplicationDbContext context)
@@ -16,11 +16,8 @@ public class DoctorPost
         if (!doctorRequest.CRM.IsNullOrEmpty())
             throw new Exception("Já existe um médico com o CRM cadastrado.");
 
-        var doctor = new Doctor
+        var doctor = new Doctor(doctorRequest.Name, doctorRequest.CRM, doctorRequest.Email)
         {
-            Name = doctorRequest.Name,
-            CRM = doctorRequest.CRM,
-            Email = doctorRequest.Email,
             CreatedDate = DateTime.Now,
             UpdateDate = DateTime.Now,
         };
